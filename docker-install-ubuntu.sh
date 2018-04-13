39aa3032d33ee6ddeb1b2d2e25636e3f4820fc81
ohrsantos <orlando.hrsantos@gmail.com>
2018-04-13 10:42:19 -0300

    First commit

---
+++ docker-install-ubuntu.sh
@@ -0,0 +1,15 @@
+apt-get remove docker docker-engine docker.io
+apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get -y install docker-ce
